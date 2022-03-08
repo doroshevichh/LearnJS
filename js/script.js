2c554cad51b7c5e5506f31702c4f63589bca2fdb
Ivan <kissatyt@gmail.com>
2022-03-08 16:41:26 +0300

    practice part 1

--- js/script.js
+++ js/script.js
@@ -18,7 +18,26 @@
 
 // alert(`Привет, ${user}`); 
 
-const a = true,
-      b = false;
+// const a = true,
+//       b = false;
 
-console.log(a && b);
+// console.log(a && b);
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+     b = prompt("Как вы его оцените?", ""),
+     c = prompt("Один из последних просмотренных фильмов", ""),
+     d = prompt("Как вы его оцените?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
