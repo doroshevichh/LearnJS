2976b8a9ae7d891c56ab986b2db6b699cd7de5b2
Ivan <kissatyt@gmail.com>
2022-03-08 15:36:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use ctrict";
+
+// const result = confirm("Are you okey?");
+// console.log(result);
+
+// const answer = -prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("Ваше имя?", "");
+// answers[1] = prompt("Ваша фамилия?", "");
+// answers[2] = prompt("Ваш возраст?", "");
+
+// console.log(answers);
+
+// const user = "Olya";
+
+// alert(`Привет, ${user}`); 
+
+const a = true,
+      b = false;
+
+console.log(a && b);
